06cdcdfbc442c26dae7c10cf39eb8fc01604455f
SveKraus <kraus.svetlana@gmx.de>
2023-06-20 12:25:09 +0200

    Initial commit

---
+++ src/Rainbow.java
@@ -0,0 +1,19 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class Rainbow {
+    public static void main(String[] args) {
+
+        Set<String> colours = new HashSet<>();
+        colours.add("Красный");
+        colours.add("Оранжевый");
+        colours.add("Жёлтый");
+        colours.add("Зелёный");
+        colours.add("Голубой");
+        colours.add("Синий");
+        colours.add("Фиолетовый");
+
+        System.out.println(colours);
+
+    }
+}
